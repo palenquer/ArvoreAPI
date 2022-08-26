ca66a05a07428cacf88afd485d834d040fd047ae
Paulo Alenquer <pauloalenquer@gmail.com>
2022-08-26 16:31:54 -0300

    created views for arvore and errors

---
+++ lib/arvore_api_web/views/arvore_view.ex
@@ -0,0 +1,23 @@
+defmodule ArvoreApiWeb.ArvoreView do
+  use ArvoreApiWeb, :view
+
+  def render("create.json", %{entity: entity}) do
+    %{
+      id: entity.id,
+      entity_type: entity.entity_type,
+      inep: entity.inep,
+      name: entity.name
+    }
+  end
+
+  def render("show.json", %{entity: entity}) do
+    %{
+      id: entity.id,
+      entity_type: entity.entity_type,
+      inep: entity.inep,
+      name: entity.name,
+      parent_id: entity.parent_id,
+      subtree_ids: Enum.map(entity.children, fn item -> item.id end)
+    }
+  end
+end
