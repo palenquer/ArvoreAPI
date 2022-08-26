84991ef78d60640148feaaf6a986ddcff76ebc3f
Paulo Alenquer <pauloalenquer@gmail.com>
2022-08-26 19:30:23 -0300

    added render view for update

--- lib/arvore_api_web/views/arvore_view.ex
+++ lib/arvore_api_web/views/arvore_view.ex
@@ -21,4 +21,14 @@ defmodule ArvoreApiWeb.ArvoreView do
       subtree_ids: Enum.map(entity.children, fn item -> item.id end)
     }
   end
+
+  def render("update.json", %{entity: entity}) do
+    %{
+      id: entity.id,
+      entity_type: entity.entity_type,
+      inep: entity.inep,
+      name: entity.name,
+      parent_id: entity.parent_id
+    }
+  end
 end
