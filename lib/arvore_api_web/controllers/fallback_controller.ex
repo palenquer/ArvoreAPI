4de01aa2ef0f47a2822f3ef3b078329ac4bfbf07
Paulo Alenquer <pauloalenquer@gmail.com>
2022-08-26 17:18:49 -0300

    fixed FallbackController errors

--- lib/arvore_api_web/controllers/fallback_controller.ex
+++ lib/arvore_api_web/controllers/fallback_controller.ex
@@ -1,10 +1,10 @@
 defmodule ArvoreApiWeb.FallbackController do
   use ArvoreApiWeb, :controller
 
-  def call(conn, {:error, %{status: status} = result}) do
+  def call(conn, {:error, error}) do
     conn
-    |> put_status(status)
+    |> put_status(400)
     |> put_view(ArvoreApiWeb.ErrorView)
-    |> render("400.json", result: result)
+    |> render("400.json", result: error)
   end
 end
