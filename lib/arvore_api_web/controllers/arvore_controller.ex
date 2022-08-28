163bc43fe35e0a292ba01f9d036790e9f79b67bb
Paulo Alenquer <pauloalenquer@gmail.com>
2022-08-28 13:28:15 -0300

    fixed controller for better understanding

--- lib/arvore_api_web/controllers/arvore_controller.ex
+++ lib/arvore_api_web/controllers/arvore_controller.ex
@@ -6,9 +6,19 @@ defmodule ArvoreApiWeb.ArvoreController do
   action_fallback ArvoreApiWeb.FallbackController
 
   def create(conn, params) do
-    params
-    |> ArvoreApi.create_entity()
-    |> handle_response(conn, "create.json", :created)
+    case params do
+      %{"entity_type" => "school"} ->
+        Entities.create_school(params)
+        |> handle_response(conn, "show.json", :created)
+
+      %{"entity_type" => "class"} ->
+        Entities.create_class(params)
+        |> handle_response(conn, "show.json", :created)
+
+      %{"entity_type" => "network"} ->
+        Entities.create_network(params)
+        |> handle_response(conn, "show.json", :created)
+    end
   end
 
   def show(conn, %{"id" => id}) do
@@ -25,16 +35,26 @@ defmodule ArvoreApiWeb.ArvoreController do
     end
   end
 
-  def update(conn, params) do
-    params
-    |> ArvoreApi.update_entity()
-    |> handle_response(conn, "update.json", :ok)
+  def update(conn, %{"id" => id} = params) do
+    case params do
+      %{"entity_type" => "school"} ->
+        Entities.update_school(id, params)
+        |> handle_response(conn, "show.json", :ok)
+
+      %{"entity_type" => "class"} ->
+        Entities.update_class(id, params)
+        |> handle_response(conn, "show.json", :ok)
+
+      %{"entity_type" => "network"} ->
+        Entities.update_network(id, params)
+        |> handle_response(conn, "show.json", :ok)
+    end
   end
 
   defp handle_response({:ok, entity}, conn, view, status) do
     conn
     |> put_status(status)
-    |> render(view, entity: entity)
+    |> render(view, entity: Entities.preload_children(entity))
   end
 
   defp handle_response({:error, _changeset} = error, _conn, _view, _status), do: error
