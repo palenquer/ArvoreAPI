ccc09dd78c6dc7f0b1d0c72b968f9c677a2a0572
Paulo Alenquer <pauloalenquer@gmail.com>
2022-08-28 13:28:42 -0300

    fixed view to only one render

--- lib/arvore_api_web/views/arvore_view.ex
+++ lib/arvore_api_web/views/arvore_view.ex
@@ -1,16 +1,6 @@
 defmodule ArvoreApiWeb.ArvoreView do
   use ArvoreApiWeb, :view
 
-  def render("create.json", %{entity: entity}) do
-    %{
-      id: entity.id,
-      entity_type: entity.entity_type,
-      inep: entity.inep,
-      name: entity.name,
-      parent_id: entity.parent_id
-    }
-  end
-
   def render("show.json", %{entity: entity}) do
     %{
       id: entity.id,
@@ -19,16 +9,6 @@ defmodule ArvoreApiWeb.ArvoreView do
       name: entity.name,
       parent_id: entity.parent_id,
       subtree_ids: Enum.map(entity.children, fn item -> item.id end)
-    }
-  end
-
-  def render("update.json", %{entity: entity}) do
-    %{
-      id: entity.id,
-      entity_type: entity.entity_type,
-      inep: entity.inep,
-      name: entity.name,
-      parent_id: entity.parent_id
     }
   end
 end
