7486c2e71d08adff6de4760f16b831b0246dc684
Paulo Alenquer <pauloalenquer@gmail.com>
2022-08-28 13:32:15 -0300

    deleted useless functions

--- lib/arvore_api.ex
+++ lib/arvore_api.ex
@@ -1,5 +1,2 @@
 defmodule ArvoreApi do
-  alias ArvoreApi.Entity
-  defdelegate create_entity(params), to: Entity.Create, as: :call
-  defdelegate update_entity(params), to: Entity.Update, as: :call
 end
